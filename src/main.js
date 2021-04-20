ffd740193cef7980b3e36f0507d1b2574d27d29d
陆唯超 <l@dpjia.com>
2021-04-20 16:06:45 +0800

    feat: 添加element

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
